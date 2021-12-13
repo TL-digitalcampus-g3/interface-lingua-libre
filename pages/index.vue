4fb22ee3ca64759a708f4270c35d009259604fd5
papaya-industry <maretvincent@protonmail.com>
2021-12-13 13:47:06 +0100

    chore: init project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ test }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Demo extends Vue {
+  test: string = 'hello world'
+}
+</script>
