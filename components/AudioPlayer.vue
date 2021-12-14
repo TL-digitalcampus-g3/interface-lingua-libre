777e9ef89f7726ad601000d5bf2147861fbca9fa
papaya-industry <maretvincent@protonmail.com>
2021-12-14 13:39:53 +0100

    feat: wip toggle play/pause

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <audio ref="audio" :src="audioUrl" controls></audio>
-    <button class="btn bg-green-300" @click="play">play</button>
+    <button @click="togglePlay">
+      <PlayIcon v-if="!isPlaying" />
+      <PauseIcon v-else />
+    </button>
     <button class="btn bg-green-300" @click="pause">pause</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop, Ref } from 'vue-property-decorator'
+import PlayIcon from '~/components/icons/Play.vue'
+import PauseIcon from '~/components/icons/Pause.vue'
 
-@Component
+@Component({ components: { PlayIcon, PauseIcon } })
 export default class AudioPlayer extends Vue {
   @Prop({ required: true }) readonly fileName!: string
-
   @Ref() readonly audio!: HTMLAudioElement
+
+  isPlaying: boolean = false
 
   get audioUrl(): string {
     return `/datas/Millars/${this.fileName}`
@@ -21,11 +27,21 @@ export default class AudioPlayer extends Vue {
 
   play(): void {
     this.$emit('played')
+    this.isPlaying = true
     this.audio.play()
   }
 
   pause(): void {
+    this.isPlaying = false
     this.audio.pause()
+  }
+
+  togglePlay(): void {
+    if (this.audio.paused) {
+      this.play()
+    } else {
+      this.pause()
+    }
   }
 }
 </script>
