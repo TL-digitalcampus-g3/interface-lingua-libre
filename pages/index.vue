efb6ea117c7cf7cf97b727681f526e344d89b3a1
papaya-industry <maretvincent@protonmail.com>
2021-12-14 12:34:02 +0100

    style: lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,36 @@
 <template>
   <div>
     <h1>{{ test }}</h1>
-    {{ records }}
-    <Test/>
-    <Collection/>
+    <Collection />
   </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
-import Test from "~/components/Test.vue";
-import Collection from "~/components/Audio/Collection.vue";
+import { Vue, Component } from 'vue-property-decorator'
+import Test from '~/components/Test.vue'
+import Collection from '~/components/Audio/Collection.vue'
 
 @Component({
-  components: {Test, Collection},
-  async asyncData({$axios}): Promise<any> {
+  components: { Test, Collection },
+  async asyncData({ $axios }): Promise<any> {
     const records = await $axios
       .$get(`datas/millars.json`)
-      .then(res => res.records)
-      .catch(error => {
+      .then((res) => res.records)
+      .catch((error) => {
         if (this.$axios.isCancel(error)) {
           console.log('Request canceled', error)
         } else {
           console.log(error)
         }
-      });
+      })
 
     return {
-      records
+      records,
     }
-  }
+  },
 })
 export default class Demo extends Vue {
-  test: string = 'hello world';
-  records: any = {};
+  test: string = 'hello world'
+  records: any = {}
 }
 </script>
