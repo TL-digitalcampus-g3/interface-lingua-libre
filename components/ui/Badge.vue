890f0e771a78192cd86fd850083b3fb534edcc67
Vincent Maret <vincent.maret@digitevent.com>
2021-12-15 15:46:49 +0100

    feat: add badge ui component

---
+++ components/ui/Badge.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="badge">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Badge extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.badge {
+  background: white;
+  border-radius: 5px;
+  display: inline-block;
+  padding: 0.2rem 0.5rem;
+  font-size: 0.9rem;
+}
+</style>
