e4c5dc60602cff629d11a54389163ef4afacccd0
Vincent Maret <vincent.maret@digitevent.com>
2021-12-15 15:37:19 +0100

    fix: class name

--- components/TagSelector.vue
+++ components/TagSelector.vue
@@ -11,7 +11,7 @@ import { Vue, Component } from 'nuxt-property-decorator'
 import { Tag } from '~/models/Record'
 
 @Component
-export default class className extends Vue {
+export default class TagSelector extends Vue {
   readonly tags: Tag[] = Object.values(Tag)
 }
 </script>
