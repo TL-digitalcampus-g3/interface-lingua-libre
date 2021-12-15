67a358d8a7d2ec061677ac10f5a1a4ef13eb5215
papaya-industry <maretvincent@protonmail.com>
2021-12-15 09:34:25 +0100

    fix: missing div

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,41 @@
 <template>
-  <div >
+  <div>
     <div class="basePage">
       <Header class="pageHeader"></Header>
       <!-- <h1>{{ test }}</h1> -->
       <div class="pageMain">
-        <Collection/>
-        <LangSwitcher/>
+        <Collection />
+        <LangSwitcher />
       </div>
-      
+
       <div class="pageBottomBar">
         <!-- <BottomBar/> -->
       </div>
     </div>
-    
-      
+  </div>
 </template>
 
 <script lang="ts">
-import {Vue, Component} from 'nuxt-property-decorator'
+import { Vue, Component } from 'nuxt-property-decorator'
 import Collection from '~/components/Audio/Collection.vue'
 import LangSwitcher from '~/components/LangSwitcher.vue'
 import Header from '~/components/Header.vue'
 
 @Component({
-  components: {LangSwitcher, Collection,Header},
+  components: { LangSwitcher, Collection, Header },
 })
-export default class Demo extends Vue {
-}
+export default class Demo extends Vue {}
 </script>
 
 <style scoped lang="scss">
-
-.basePage{
+.basePage {
   @apply grid h-screen;
   grid-template-columns: 5rem auto;
   grid-template-rows: auto 5rem;
   column-gap: 20px;
 }
 
-.pageHeader{
+.pageHeader {
   @apply shadow-lg bg-backgroundBlock-light;
 }
 
@@ -46,12 +43,9 @@ export default class Demo extends Vue {
 
 // }
 
-.pageBottomBar{
-  @apply bg-primary shadow-lg ;
+.pageBottomBar {
+  @apply bg-primary shadow-lg;
   height: 80px;
   grid-column: 1 / 3;
-
-  
 }
-
 </style>
