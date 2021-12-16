d050fa8d44aed9facac934be079acfbd3a55a7a1
Michael R <tekmedias@gmail.com>
2021-12-17 00:23:22 +0100

    feat(layout): prepare default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <Nuxt/>
+</template>
+
+<script lang="ts">
+export default {
+  head() {
+    return {
+      title: `Lingua Libre`,
+      meta: [
+        {hid: 'description', name: 'description', content: 'Lingua Libre audios verification interface'}
+      ]
+    }
+  }
+}
+</script>
