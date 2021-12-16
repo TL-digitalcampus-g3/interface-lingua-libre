40a56dc4afe31d3275524cc2334f3a8a8754de52
Michael R <tekmedias@gmail.com>
2021-12-16 17:02:29 +0100

    feat(tags): enable active tag style on action bar

--- components/TagSelector/index.vue
+++ components/TagSelector/index.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="badges">
     <button v-for="tag in tags" :key="tag" class="mx-1" @click="setTag(tag)">
-      <TagBadge :tag="tag" />
+      <TagBadge :tag="tag" :class="[
+        {'badge--active' : (currentTag === tag)}
+      ]"/>
     </button>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import {Vue, Component} from 'nuxt-property-decorator'
 import TagBadge from './TagBadge.vue'
-import { Tag, RecordT } from '~/models/Record'
-import { TagMutationPayload } from '~/store'
+import {Tag, RecordT} from '~/models/Record'
+import {TagMutationPayload} from '~/store'
 
-@Component({ components: { TagBadge } })
+@Component({components: {TagBadge}})
 export default class TagSelector extends Vue {
   readonly tags: Tag[] = Object.values(Tag)
+
+  currentTag: Tag | null = null
 
   get activeAudio(): RecordT['fileName'] {
     return this.$store.state.activeAudio
   }
 
   setTag(tag: Tag): void {
+    this.currentTag = tag
     if (this.activeAudio) {
-      const payload: TagMutationPayload = { fileName: this.activeAudio, tag }
+      const payload: TagMutationPayload = {fileName: this.activeAudio, tag}
       this.$store.dispatch('setTag', payload)
     }
   }
@@ -33,5 +38,10 @@ export default class TagSelector extends Vue {
 .badges {
   display: flex;
   justify-content: center;
+
+  .badge--active {
+    background: white !important;
+  }
 }
+
 </style>
